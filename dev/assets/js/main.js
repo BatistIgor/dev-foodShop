6c8dac0dfc49155cf308aa2c56302675f68885b7
BatistIgot <igorbatist089@gmail.com>
2024-09-25 16:40:54 +0300

    created build version v.1.0

--- dev/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector('.hamburger')
-const navigation = document.querySelector('.header__navigation')
-
-button.addEventListener('click', ()=> {
-    button.classList.toggle("is-active");
-    navigation.classList.toggle("header__navigation--active");
-})
